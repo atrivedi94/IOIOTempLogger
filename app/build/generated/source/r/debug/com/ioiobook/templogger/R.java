2d2f8240fec0a1fff4aafdc889497c66dd149e82
Akanksha Trivedi <atrivedi@ucdavis.edu>
2016-03-30 10:47:19 -0700

    IT WORKS - need to calibrate temp sensor?

--- app/build/generated/source/r/debug/com/ioiobook/templogger/R.java
+++ app/build/generated/source/r/debug/com/ioiobook/templogger/R.java
@@ -14,17 +14,21 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int lastLogLine=0x7f050005;
-        public static final int logButton=0x7f050004;
-        public static final int radio_c=0x7f050003;
-        public static final int radio_f=0x7f050002;
-        public static final int temp=0x7f050001;
-        public static final int title=0x7f050000;
+        public static final int lastLogLine=0x7f060005;
+        public static final int logButton=0x7f060004;
+        public static final int radio_c=0x7f060003;
+        public static final int radio_f=0x7f060002;
+        public static final int temp=0x7f060001;
+        public static final int title=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+    }
+    public static final class xml {
+        public static final int accessory_filter=0x7f040000;
+        public static final int device_filter=0x7f040001;
     }
 }
